04ce64272c3af77ec40ecf2b5ddb909470a94b48
James Jerram <jamesjerram4@gmail.com>
2015-04-15 16:00:54 -0400

    added database connection code to connect page, created credentials file

--- connect.php
+++ connect.php
@@ -4,6 +4,17 @@
 		<?php
 			if(isset($_GET["sid"])){
 				//database calls to generate a custom URI to be handled by the client program go here
+				$servername = "localhost";
+				//make sure credentials file is hidden in .gitignore .htaccess
+				$credentials = fopen("config/db_credentials.txt", "r");
+				$username = trim(fgets($credentials));
+				$password = trim(fgets($credentials));
+				$conn = new mysqli($servername, $username, $password);
+				
+				if($conn->connect_error) {
+					die("Database connection error.");
+				}
+				
 				//header( 'Location: XXXX://' . $uri ) ;
 			}
 		?>
